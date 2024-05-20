87bfc7983380c3a8cbdd907d5f71678246a3302e
JWest2323 <thejamalwest@gmail.com>
2024-05-20 15:47:27 -0500

    TweetCard GFE component impl

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import ProgressBar from "./components/ProgressBar";
 import TemperatureConverter from "./components/TemperatureConverter";
+import TweetCard from "./components/TweetCard";
 
 function App() {
   return (
@@ -9,6 +10,39 @@ function App() {
       <ProgressBar completionValue={30} />
       <ProgressBar completionValue={50} />
       <TemperatureConverter />
+
+      <div className="tweet-list" >
+        <TweetCard
+          profileImage="https://randomuser.me/api/portraits/thumb/men/75.jpg"
+          name="John Doe"
+          handle="johndoe"
+          date="Dec 25"
+          message="I got my wife a fridge for Christmas. I can't wait to see her face light up when she opens it."
+          replyCount="1,094"
+          retweetCount="512"
+          likeCount="512"
+        />
+        <TweetCard
+        profileImage="https://xsgames.co/randomusers/avatar.php?g=female"
+        name="Jane Doe"
+        handle="janedoe"
+        date="Oct 24"
+        message="I told my husband that he has no sense of direction at all. He got so mad that he packed up his stuff and right."
+        replyCount="193"
+        retweetCount="3,960"
+        likeCount="40.5 K"
+      />
+      <TweetCard
+        profileImage="https://xsgames.co/randomusers/avatar.php?g=pixel"
+        name="WALL-E"
+        handle="walle"
+        date="Jul 1"
+        message="The best way to predict the future is to invent it."
+        replyCount="193"
+        retweetCount="3,960"
+        likeCount="40.5 K"
+      />
+      </div>
     </>
   );
 }
